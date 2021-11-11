9adb53e4e55f837f1de600bf454795d5f4b0ccb1
xyf-mac <xu_yanfeng@126.com>
2021-11-12 00:04:58 +0800

    更新popup options

--- source/src/options/index.vue
+++ source/src/options/index.vue
@@ -11,8 +11,9 @@
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
+
 @Component({
-  components: {PuzzleBoard},
+  components: {},
 })
 export default class Index extends Vue {
   name: string = "index";
