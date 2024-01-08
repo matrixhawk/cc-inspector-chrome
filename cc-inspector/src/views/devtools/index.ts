686b6d11add4d6a2a8ccb58dc5fdce6a48df7e81
xu_yanfeng <xu_yanfeng@126.com>
2024-01-08 19:59:08 +0800

    将devtools的代码移植过去

--- cc-inspector/src/views/devtools/index.ts
+++ cc-inspector/src/views/devtools/index.ts
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
+// 用的是ui/index.vue
 import App from "./index.vue";
 import CCP from "cc-plugin/src/ccp/entry-render";
 import pluginConfig from "../../../cc-plugin.config";
 import ccui from "@xuyanfeng/cc-ui";
 import "@xuyanfeng/cc-ui/dist/ccui.css";
 import "@xuyanfeng/cc-ui/iconfont/iconfont.css";
-
+import {init} from './register-panel';
+init();
 export default CCP.init(pluginConfig, {
   ready: function (rootElement: any, args: any) {
     const app = createApp(App);
