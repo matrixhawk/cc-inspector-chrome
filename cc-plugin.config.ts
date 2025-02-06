1d94f11ab4c062b77609dc8dceb376654aa7850d
xu_yanfeng <xu_yanfeng@126.com>
2025-02-06 15:52:46 +0800

    2.1.6

--- cc-plugin.config.ts
+++ cc-plugin.config.ts
@@ -9,7 +9,7 @@ function i18n(key: string) {
 
 const manifest: CocosPluginManifest = {
   name: pkgName,
-  version: "2.1.5",
+  version: "2.1.6",
   description: "Debug games made with CocosCreator and display node trees and node properties",
   author: "xu_yanfeng",
   main: "./src/main.ts",
@@ -54,7 +54,7 @@ const manifest: CocosPluginManifest = {
   },
 };
 const options: CocosPluginOptions = {
-  obscure: true,
+  obscure: false,
   server: {
     enabled: false,
     port: 2022,
